f3af26e9fe4d2d843c6c4704d8318d924b197331
selvarajks <selvarajks@gmail.com>
2019-12-16 14:51:26 +0800

    Bethany first commit

--- BethanyPieShop/Migrations/20191216041220_BethanyAzure.Designer.cs
+++ BethanyPieShop/Migrations/20191216041220_BethanyAzure.Designer.cs
@@ -4,14 +4,16 @@ using BethanyPieShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BethanyPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191216041220_BethanyAzure")]
+    partial class BethanyAzure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
